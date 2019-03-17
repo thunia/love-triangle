77eabb1a43f89b19746f41604814315d35dc7157
Irina Kotova <irina.lushagina@gmail.com>
2019-03-17 23:36:26 +0300

    feat: getLoveTrianglesCount function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 /**
- * @param preferences - an array of integers. Indices of people, whom they love
+ * @param preferences - an preferences of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+module.exports = function getLoveTrianglesCount(preferences) {
+  var trianglesArray = [],
+      countOfTriangles = 0; 
+  
+  for(var i=0; i < preferences.length; i++){
+    var n = i + 1,
+        k = preferences[n - 1],
+        m = preferences[k - 1];
+        
+      if(trianglesArray.indexOf(n) < 0 && k != n){
+        
+        if(preferences[m - 1] == n){         
+         countOfTriangles = countOfTriangles + 1;
+         trianglesArray.push(n,k,m);  
+        }
+      }      
+  }  
+  return countOfTriangles;
 };
